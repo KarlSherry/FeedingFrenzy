9ba3f2482e33864078666cef27c23bd2078fe44f
Karl Sherry <karl.sherry@gmail.com>
2014-03-31 18:25:38 +0100

    first commit

---
+++ Assets/ForceController.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ForceController : MonoBehaviour {
+	
+	Vector3 force = Vector3.zero;
+	Vector3 velocity = Vector3.zero;
+	Vector3 target = Vector3.zero;
+	
+	public float maxSpeed = 25.0f;
+	public float mass = 1.0f;
+	
+	// Use this for initialization
+	void Start () {
+		target = Random.insideUnitSphere * 10;
+		target.x -= 220;
+		target.y = -10;
+		target.z += 330;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		force += Seek ();
+		
+		Vector3 acceleration = force / mass;
+		
+		velocity += acceleration * Time.deltaTime;
+		transform.position += velocity * Time.deltaTime;
+		
+		force = Vector3.zero;
+		
+		if (velocity.magnitude > float.Epsilon) {
+			transform.forward = velocity.normalized;
+		}
+		
+		velocity *= 0.99f;
+		
+		//Debug.DrawLine (transform.position, target, Color.black);
+	}
+
+	Vector3 Seek () {
+		Vector3 desiredVelocity = target - transform.position;
+		float distToTarget = desiredVelocity.magnitude;
+
+		if (distToTarget < 2) {
+			target = Random.insideUnitSphere * 10;
+			target.x -= 220;
+			target.y = -10;
+			target.z += 330;
+		}
+
+		desiredVelocity.Normalize ();
+		desiredVelocity *= maxSpeed;
+		
+		return desiredVelocity - velocity;
+	}
+	
+	Vector3 ArriveToTarget() {
+		Vector3 toTarget = target - transform.position;
+		
+		float distToTarget = toTarget.magnitude;
+		float slowingDistance = 8.0f;
+		
+		if (distToTarget < 1) {
+			target = Random.insideUnitSphere * 10;
+		}
+		
+		float decelerationTweaker = maxSpeed / 5;
+		float rampedSpeed = maxSpeed * (distToTarget / (slowingDistance * decelerationTweaker));
+		
+		float clampedSpeed = Mathf.Min (rampedSpeed, maxSpeed);
+		
+		Vector3 desiredVelocity = clampedSpeed * (toTarget / distToTarget);
+		
+		return desiredVelocity - velocity;
+	}
+}
---
+++ Assets/SteeringBehaviours.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SteeringBehaviours : MonoBehaviour {
+	
+	Vector3 force = Vector3.zero;
+	Vector3 velocity = Vector3.zero;
+	
+	public float maxSpeed = 5.0f;
+	public float mass = 1.0f;
+	
+	int currentWaypoint = 0;
+	
+	List<Vector3> wayPoints = new List<Vector3>();
+	
+	// Use this for initialization
+	void Start () {
+		CreatePath (10, 30);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		force += FollowPath ();
+		
+		//DrawPath ();
+		
+		Vector3 acceleration = force / mass;
+		
+		velocity += acceleration * Time.deltaTime;
+		transform.position += velocity * Time.deltaTime;
+		
+		force = Vector3.zero;
+		
+		if (velocity.magnitude > float.Epsilon) {
+			transform.forward = velocity.normalized;
+		}
+		
+		velocity *= 0.99f;
+	}
+	
+	private void CreatePath (int points, float radius) {
+		float thetaInc = (2 * Mathf.PI) / points;
+		
+		for (int i = 0; i < points; i++) 
+		{			
+			float theta = thetaInc * i;
+			
+			float x = Mathf.Sin(theta) * radius;
+			float z = Mathf.Cos(theta) * radius;
+			
+			wayPoints.Add(new Vector3(x - 210, -10, z + 330));
+		}
+	}
+	
+	private void DrawPath() {
+		float maxPoints = wayPoints.Count;
+		
+		for (int i = 0; i < maxPoints; i++) 
+		{
+			int nextPos = i + 1;
+			
+			if(nextPos >= maxPoints)
+			{
+				nextPos = 0;
+			}
+			
+			Vector3 startPos = wayPoints[i];
+			Vector3 endPos = wayPoints[nextPos];
+			
+			Debug.DrawLine (startPos, endPos, Color.red);
+		}
+	}
+	
+	Vector3 Seek (Vector3 point) {
+		Vector3 desiredVelocity = point - transform.position;
+		desiredVelocity.Normalize ();
+		desiredVelocity *= maxSpeed;
+		
+		return desiredVelocity - velocity;
+	}
+	
+	Vector3 FollowPath() {
+		Vector3 currentTarget = wayPoints [currentWaypoint];
+		
+		float distToTarget = (currentTarget - transform.position).magnitude;
+		
+		if (distToTarget < 1.0f) {
+			currentWaypoint ++;
+		}
+		if (currentWaypoint >= wayPoints.Count) {
+			currentWaypoint = 0;
+		}
+		
+		return Seek(currentTarget);
+	}
+}
