dd4867a35ffc068ab98c37b0f400fa0957970f01
Karl Sherry <karl.sherry@gmail.com>
2014-03-31 18:43:56 +0100

    second commit

---
+++ Assets/TroutSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TroutSpawner : MonoBehaviour {
+
+	public GameObject fish;
+
+	public int numberOfFish = 0;
+
+	List<GameObject> fishList = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < numberOfFish; i++) 
+		{
+			fishList.Add (fish);
+		}
+	}
+}
