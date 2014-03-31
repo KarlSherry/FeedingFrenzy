47699a7465087be5950f9f615648f4620172e612
Karl Sherry <karl.sherry@gmail.com>
2014-03-31 19:35:10 +0100

    fish spawner working

--- Assets/TroutSpawner.cs
+++ Assets/TroutSpawner.cs
@@ -6,20 +6,21 @@ public class TroutSpawner : MonoBehaviour {
 
 	public GameObject fish;
 
-	public int numberOfFish = 0;
+	public int numberOfFish = 10;
 
 	List<GameObject> fishList = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
-	
+		for (int i = 0; i < numberOfFish; i++) 
+		{
+			GameObject fishClone = Instantiate(fish, transform.position, transform.rotation) as GameObject; 
+			fishList.Add (fishClone);
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		for (int i = 0; i < numberOfFish; i++) 
-		{
-			fishList.Add (fish);
-		}
+
 	}
 }
